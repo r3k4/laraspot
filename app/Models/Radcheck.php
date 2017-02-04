e34fc593b6a1b8b4b0eff1e9d5d83a91372ccd25
reka prihatanto <rey.barrolz@gmail.com>
2017-02-05 01:20:10 +0700

    rubah password via menu profile

---
+++ app/Models/Radcheck.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Radcheck extends Model
+{
+    protected $table = 'radcheck';
+    public $incrementing = false;
+    public $timestamps = false;
+
+
+    protected $fillable = [
+    	'username',
+    	'attribute',
+    	'op',
+    	'value'
+    ];
+
+    /**
+     * query scopes
+     */
+    
+    public function scopeUsername($query, $username = null)
+    {
+    	if($username == null){
+    		return $query->whereUsername(auth()->user()->username);
+    	}
+    	return $query->whereUsername($username);
+    }
+
+
+
+}
--- app/Services/Users/updateUserPasswordService.php
+++ app/Services/Users/updateUserPasswordService.php
@@ -2,12 +2,21 @@
 
 namespace App\Services\Users;
 
+
+use App\Models\Radcheck;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class updateUserPasswordService 
 {
 
 	use ValidatesRequests;
+
+	protected $radcheck;
+
+	public function __construct(Radcheck $radcheck)
+	{
+		$this->radcheck = $radcheck;
+	}
 
 
 	public function handle()
@@ -19,16 +28,19 @@ class updateUserPasswordService
         ]);
 
         if (\Hash::check(request()->password_lama, auth()->user()->password)) {
-            auth()->user()->whereId(auth()->user()->id)->update(['password' => request()->password]);
-            return back()->with('pesan', 'password telah ter-update');
+            auth()->user()->whereId(auth()->user()->id)->update(['password' => bcrypt(request()->password)]);
         }
         // end of password system
 
         // password hotspot
-        
+        $u = $this->radcheck->username()->where('attribute', '=', 'Cleartext-Password')->first();
+        $u->value = request()->password;
+        $u->save();
+        return back()->with('pesan', 'password telah tersimpan');
+
         // end of password hotspot 
 
-        return back()->with('pesan_error', 'password gagal ter-update');
+        // return back()->with('pesan_error', 'password gagal ter-update');
 	}	
 
 
